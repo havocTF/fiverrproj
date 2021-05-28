bae9d5f1491d1cca90eaec7859ee239afedeaf8b
havocchan <wilsonxfeng@gmail.com>
2021-05-28 08:18:55 -0600

    a
    a

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("--mute-audio")
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
 browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
@@ -18,12 +19,13 @@ count = 1
 
 def changeServerAndJoin():
     try:
-        element = browser.find_element_by_id("server-dropdown-container")
+        element = browser.find_element_by_id("server-dropdown-container")  # looks for the dropdown for the servers
         element.click()
         time.sleep(1)
-        element = browser.find_element_by_id("server-option-3")
+        element = browser.find_element_by_id(
+            "server-option-3")  # looks for the eu- 2 option (will only choose the 4th option)
         element.click()
-        element = browser.find_element_by_id("play-game-button")
+        element = browser.find_element_by_id("play-game-button")  # looks for the play button
         element.click()
     except:
         changeServerAndJoin()
@@ -32,16 +34,19 @@ def changeServerAndJoin():
 def wait():
     WebDriverWait(browser, 900).until(
         EC.presence_of_element_located((By.ID, "server-dropdown-container"))
+        # this just waits for the server dropdown to be present
     )
 
 
-browser.get('https://beta.modd.io/play/two-houses')
+browser.get('https://beta.modd.io/play/two-houses')  # this loads the url
 wait()
 changeServerAndJoin()
-while True:
+while count > 49:  # until theres 50 bots
     browser.execute_script("window.open('about:blank', 'tab{}');".format(count))
-    browser.switch_to.window("tab{}".format(count))
-    browser.get('https://beta.modd.io/play/two-houses')
+    browser.switch_to.window("tab{}".format(count))  # these 2 lines just open new tab
+    browser.get('https://beta.modd.io/play/two-houses')  # opens up url
     wait()
     changeServerAndJoin()
     count = count + 1
+input("finished botting... Don't close this window or else it will stop communication with the server hoster and "
+      "close all the bot tabs")
---
+++ run.bat
@@ -0,0 +1,2 @@
+cd %CD%
+python main.py
