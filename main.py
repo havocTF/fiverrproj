cb2915d04b95540579b8a84c42bceda84e115f3c
havocTF <62627746+havocchan@users.noreply.github.com>
2021-05-28 15:34:13 -0600

    a

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-import os
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument("--mute-audio")
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--log-level=3")
+chrome_options.add_argument('--disable-extensions')
+chrome_options.add_argument('--disable-gpu')
 
 browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 count = 1
 othercount = 1
+
+browser.maximize_window()
 
 
 def changeServerAndJoin():
